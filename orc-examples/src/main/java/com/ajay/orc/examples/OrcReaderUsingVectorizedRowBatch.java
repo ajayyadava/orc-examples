0bb33816bcfd9b9a36a10d80adfc2b754c090ec7
Ajay Yadava <ajayyadava@apache.org>
2017-07-19 18:01:32 -0700

    Read metadata from the file

--- orc-examples/src/main/java/com/ajay/orc/examples/OrcReaderUsingVectorizedRowBatch.java
+++ orc-examples/src/main/java/com/ajay/orc/examples/OrcReaderUsingVectorizedRowBatch.java
@@ -1,6 +1,7 @@
 package com.ajay.orc.examples;
 
 
+import java.util.Arrays;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
@@ -23,11 +24,42 @@ public class OrcReaderUsingVectorizedRowBatch {
           .createReader(new Path("/tmp/simple-my-file.orc"), OrcFile.readerOptions(conf));
 
       // read row batch column by column and then print it out
+      System.out.println("CompressionKind = " + reader.getCompressionKind());
+      System.out.println(" ============================================== " );
+      System.out.println("CompressionSize = " + reader.getCompressionSize());
+      System.out.println(" ============================================== " );
+      System.out.println("ContentLength = " + reader.getContentLength());
+      System.out.println(" ============================================== " );
+      System.out.println("fileTail = " + reader.getFileTail());
+      System.out.println(" ============================================== " );
 
+      // FileVersion , major and minor
+      System.out.println(String
+          .format("fileVersion:\n name: {}, major: {}, minor: {} fileVersion = ",
+              reader.getFileVersion().getName(), reader.getFileVersion().getMajor(),
+              reader.getFileVersion().getMinor()));
+      System.out.println(" ============================================== " );
+
+      System.out.println("postscript = " + reader.getFileTail().getPostscript());
+      System.out.println(" ============================================== " );
+      System.out.println("footer = " + reader.getFileTail().getFooter());
+      System.out.println(" ============================================== " );
+      System.out.println("no. of rows = " + reader.getNumberOfRows());
+      System.out.println(" ============================================== " );
+      System.out.println("raw data size of cols = " + reader.getRawDataSizeOfColumns(Arrays.asList("x", "y")));
+      System.out.println(" ============================================== " );
+      System.out.println("data size from col indices = " + reader.getRawDataSizeFromColIndices(Arrays.asList(0, 1)));
+      System.out.println(" ============================================== " );
+
+      System.out.println("metadata keys = " + reader.getMetadataKeys());
+      System.out.println(" ============================================== " );
+      System.out.println("reader.getStatistics() = " + reader.getStatistics());
+      System.out.println(" ============================================== " );
       RecordReader recordReader = reader.rows();
 
       // read schema from the file and get a row batch buffer
       VectorizedRowBatch batch = reader.getSchema().createRowBatch();
+
 
       // read the next batch in the buffer 'batch'
       while (recordReader.nextBatch(batch)) {
@@ -40,7 +72,9 @@ public class OrcReaderUsingVectorizedRowBatch {
           totalY += colY.vector[i];
         }
       }
-      // how can you read the meta???
+
+
+
     } catch (Exception e) {
 
     }
